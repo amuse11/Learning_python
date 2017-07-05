610535158428068431d117aeb78c375e7f215df8
amuse11 <amuse11@outlook.com>
2017-07-05 02:32:59 +0100

    Update Finds_the_surface_area_of_a_sphere.py

--- Finds_the_surface_area_of_a_sphere.py
+++ Finds_the_surface_area_of_a_sphere.py
@@ -1,7 +1,7 @@
 #This is a simple program that allows the user to enter a radius for a sphere and output the surface area of that sphere using the radius that was input
 import math
 
-def area(r):
+def area_sphere(r):
     """Returns the area of a sphere with radius r"""
     a = 4 * math.pi * r**2
     return a
@@ -10,6 +10,6 @@ r = input()
 
 try:        #This will convert the input of r into a float to see if it can be computed, if it cannot i.e. it is not a number, then the program ends
     r = float(r)
-    print (area(r))
+    print (area_sphere(r))
 except ValueError:
     print("That's not a number!")
