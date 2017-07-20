fd6fb555c1eb5e7ea4b2d26639c51ad3298d12ba
amuse11 <amuse11@outlook.com>
2017-07-20 15:20:06 +0100

    Updated Flip_a_coin.py to include a while loop
    Included a while loop so that the user may play the game infinitely until they type "End".

--- Flip_a_coin.py
+++ Flip_a_coin.py
@@ -1,11 +1,18 @@
 import random
 
-print ("Pick a side of the coin. Heads or Tails?")
-input_coin = input()
-input_coin = input_coin.lower()
-coin = random.choice(["heads", "tails"])
-
-if input_coin == coin:
-    print ("You picked the right side!")
-else:
-    print ("Sadly, that is not the right side!")
+while True:
+    print("Pick a side of the coin. Heads or Tails? Type 'End' when you wish to stop.")
+    input_coin = input()
+    input_coin = input_coin.lower()
+    coin = random.choice(["heads", "tails"])
+    if input_coin == coin:
+        print ("The side is " + (coin))
+        print ("You picked the right side!")
+        continue
+    elif input_coin != coin and input_coin != "end":
+        print ("The side is " + (coin))
+        print ("Sadly, that is not the right side!")
+        continue
+    elif input_coin == "end":
+        print ("The program has ended")
+        break
