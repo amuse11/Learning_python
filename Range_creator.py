bd3a1e2ca1a457003c107ece67fdc490a36db967
amuse11 <amuse11@outlook.com>
2017-07-20 16:00:51 +0100

    Updated Range_creator.py with a while loop check
    Added a while loop that checks if the end of the list is smaller than the start of the list, if it is, the program tells the user to choose a different end of the list.

--- Range_creator.py
+++ Range_creator.py
@@ -2,4 +2,8 @@ start = int(input("What will be the start of the list?" ))
 stop = int(input("What will be the end of the list? ")) + 1
 step = int(input("What do you want each number to be incremented by? "))
 
+while stop < start:
+    print ("The end of the list is smaller than the start, choose a different end of the list, please.")
+    stop = int(input("What will be the end of the list? ")) + 1
+
 print(list(range(start,stop, step)))
