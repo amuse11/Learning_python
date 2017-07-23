ce02d44642f0f15a235185d18cfeefc5b79927fa
amuse11 <amuse11@outlook.com>
2017-07-23 02:52:25 +0100

    Update Finds_the_volume_of_a_cone.py
    Allows the user to input units and writes the unit out along with the end value, as well as changing the error message to be more grammatically correct.

--- Finds_the_volume_of_a_cone.py
+++ Finds_the_volume_of_a_cone.py
@@ -4,13 +4,14 @@ def volume_cone(r, h):
     v = math.pi * r**2 * h/3
     return v
 
-print ("Please input the radius and height of the cone.")
+print ("Please input the radius and height of the cone, and the units")
 r = input()
 h = input()
+units = input()
 
 try:
     r = float(r)
     h = float(h)
-    print (volume_cone(r, h))
+    print((volume_cone(r, h)), str(units))
 except ValueError:
-    print("That's not a number!")
+    print("The radius and/or height is not a number!")
