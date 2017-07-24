69880e2621a93e5a2464352b0afc38a4359ccb60
amuse11 <amuse11@outlook.com>
2017-07-24 06:04:53 +0100

    Update Students_grades.py
    Used with and as as it'd be much simpler.

--- Students_grades.py
+++ Students_grades.py
@@ -1,4 +1,3 @@
-
 class Students(object):
     def __init__ (self, student, marks):
         self.student = student
@@ -6,40 +5,33 @@ class Students(object):
 
 class_report = open("Class_report.txt", "a+")
 
-finish = "Finish"
+with open("Class_report.txt", "a+") as class_report:
+    while True:
+        student = input("Enter a student name, or press enter to exist").strip()
 
+        if not student:
+            break
 
-while finish != "end":
-    finish = input("Type 'end' when you want the program to end, otherwise, just click enter.")
-    finish = finish.lower()
+        print ("How many marks did the student get in this exam?")
+        marks = input()
+        marks = int(marks) 
+        if marks >= 80:
+            marks = str(marks)
+            marks = "an A"
+        elif marks >= 70:
+            marks = str(marks)
+            marks = "a B"
+        elif marks >= 60:
+            marks = str(marks)
+            marks = "a C"
+        elif marks >= 50:
+            marks = str(marks)
+            marks = "a D"
+        elif marks < 50 and marks >= 0:
+            marks = str(marks)
+            marks = "a Fail"
 
-    if finish == "end":
-        break
-    
-    print ("What is the name of this student?")
-    student = input()
+        pupil = Students(student, marks)
+        class_report.write(("%s achieved %s" % (pupil.student, pupil.marks)) + "\n")
+        
 
-
-    print ("How many marks did the student get in this exam?")
-    marks = input()
-    marks = int(marks) 
-    if marks >= 80:
-        marks = str(marks)
-        marks = "an A"
-    elif marks >= 70:
-        marks = str(marks)
-        marks = "a B"
-    elif marks >= 60:
-        marks = str(marks)
-        marks = "a C"
-    elif marks >= 50:
-        marks = str(marks)
-        marks = "a D"
-    elif marks < 50 and marks >= 0:
-        marks = str(marks)
-        marks = "a Fail"
-
-    pupil = Students(student, marks)
-    class_report.write(("%s achieved %s" % (pupil.student, pupil.marks)) + "\n")
-    
-class_report.close()
